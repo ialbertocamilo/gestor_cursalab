dda93287b3a4845b65961755510b6c1944ac8d38
Jean Carlo <jeanca2198@gmail.com>
2022-07-05 12:33:57 -0500

    Migration videoteca, checklists, meetings, user uploads, media

--- database/migrations/2022_07_05_112817_create_checklist_answers_table.php
+++ database/migrations/2022_07_05_112817_create_checklist_answers_table.php
@@ -16,12 +16,12 @@ return new class extends Migration
         Schema::create('checklist_answers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('checklist_id')->nullable()->constrained('checklists');
-            $table->foreignId('entrenador_id')->nullable()->constrained('users');
-            $table->foreignId('alumno_id')->nullable()->constrained('users');
-            $table->foreignId('curso_id')->nullable()->constrained('courses');
-            $table->foreignId('categoria_id')->nullable()->constrained('schools');
+            $table->foreignId('coach_id')->nullable()->constrained('users');
+            $table->foreignId('student_id')->nullable()->constrained('users');
+            $table->foreignId('course_id')->nullable()->constrained('courses');
+            $table->foreignId('school_id')->nullable()->constrained('schools');
 
-            $table->decimal('porcentaje', 4,2)->nullable();
+            $table->decimal('percent', 4,2)->nullable();
 
             $table->timestamps();
             $table->softDeletes();
--- database/migrations/2022_07_05_113229_create_checklist_items_table.php
+++ database/migrations/2022_07_05_113229_create_checklist_items_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('checklist_id')->nullable()->constrained('checklists');
 
-            $table->string('actividad');
+            $table->string('activity');
 
             $table->tinyInteger('position')->nullable();
 
