88588b13ce2f4bb3a110732ae6b83f335e5784a7
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2022-07-06 09:56:35 -0500

    course code

--- database/migrations/2022_07_01_123442_create_courses_table.php
+++ database/migrations/2022_07_01_123442_create_courses_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->nullable();
+            $table->string('external_code')->nullable();
             $table->string('description')->nullable();
 
             $table->string('imagen')->nullable();
