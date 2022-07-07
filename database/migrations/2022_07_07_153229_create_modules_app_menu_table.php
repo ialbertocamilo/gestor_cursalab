6d8690c06086e255b0cf50bfffb265c2835e4384
Jean Carlo <jeanca2198@gmail.com>
2022-07-07 15:37:13 -0500

    Some additions and fixes to migrations

---
+++ database/migrations/2022_07_07_153229_create_modules_app_menu_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modules_app_menu', function (Blueprint $table) {
+            $table->foreignId('module_id')->nullable()->constrained('criterion_value');
+            $table->foreignId('menu_id')->nullable()->constrained('taxonomies');
+
+            $table->tinyInteger('position')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modules_app_menu');
+    }
+};
