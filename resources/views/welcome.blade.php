73760a9a6052b576bd8cd52d3ce36de9b58b46b1
deyvijames <deyvi@lamediadl.com>
2022-08-19 17:44:50 -0500

    welcome estilos 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
 
     @php
         $workspace = session('workspace');
-        $workspace_logo = is_array($workspace) ? $workspace['logo'] : "img/logo_cursalab.png";
     @endphp
     <div style="
             height: 100vh;
@@ -17,10 +16,10 @@
         <!-- <div class="mb-5 mx-auto">
             <img src="img/logo_cursalab.png" alt="..." class="img-fluid" width="230">
         </div> -->
-        <h2>Bienvenido(a) a </h2>
+        <h2>Bienvenido(a) a {{ $workspace['name']}}</h2>
         <p style="font-size:large;">Configura tu contenido utilizando el menú lateral.</p>
         <div class="mt-5 mx-auto">
-            <img src="{{ $workspace_logo }}" alt="" width="350">
+            <img src="img/welcome_gestor.svg" alt="" width="350">
         </div>
     </div>
 @endsection
