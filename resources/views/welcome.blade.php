8864da456599475446f253f77f27a83b170b7a54
deyvijames <deyvi@lamediadl.com>
2022-08-19 17:33:28 -0500

    Estilos welcome 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,15 @@
             width: 100%;
             display: flex;
             justify-content: center;
-            align-items: center">
+            align-items: center;
+            flex-flow: column;">
         <!-- <div class="mb-5 mx-auto">
             <img src="img/logo_cursalab.png" alt="..." class="img-fluid" width="230">
         </div> -->
         <h2>Bienvenido(a) a WeConnect 2.0</h2>
         <p style="font-size:large;">Configura tu contenido utilizando el menú de la izquierda.</p>
         <div class="mt-5 mx-auto">
-            <img src="img/welcome_gestor.svg" alt="" width="230">
+            <img src="img/welcome_gestor.svg" alt="" width="350">
         </div>
     </div>
 @endsection
