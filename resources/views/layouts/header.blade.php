430c17da67a132a6141be60229009976f8bd78c4
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2022-08-20 11:35:50 -0500

    media url fileservice

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="all,follow">
-    <meta name="BUCKET_BASE_URL" content="{{ env('BUCKET_BASE_URL', '')  }}">
+    <meta name="BUCKET_BASE_URL" content="{{ App\Services\FileService::generateUrl('/') }}">
 
     <!-- Bootstrap CSS-->
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
