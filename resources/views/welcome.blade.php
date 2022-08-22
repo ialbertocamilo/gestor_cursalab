8e5d71af5414bb5f8382f755ea6adc3a4750e10d
Deyvi <deyvi@lamediadl.com>
2022-08-22 13:07:35 -0500

    Seeder admins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,10 +16,10 @@
         <!-- <div class="mb-5 mx-auto">
             <img src="img/logo_cursalab.png" alt="..." class="img-fluid" width="230">
         </div> -->
-        <h2>Bienvenido(a) a {{ $workspace['name']}}</h2>
+        <h2>Bienvenido(a) al espacio de administración</h2>
         <p style="font-size:large;">Configura tu contenido utilizando el menú lateral.</p>
         <div class="mt-5 mx-auto">
-            <img src="img/welcome_gestor.svg" alt="" width="350">
+            <img src="img/welcome_gestor.svg" alt="" width="380">
         </div>
     </div>
 @endsection
