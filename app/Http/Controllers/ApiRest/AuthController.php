fdbb24b64e71c5568f7ff57ebfa3cbdeeef3948c
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2022-08-25 16:58:02 -0500

    logout api fix test

--- app/Http/Controllers/ApiRest/AuthController.php
+++ app/Http/Controllers/ApiRest/AuthController.php
@@ -10,6 +10,8 @@ use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
+use Illuminate\Http\Request;
+
 class AuthController extends Controller
 {
     public function login(LoginAppRequest $request)
