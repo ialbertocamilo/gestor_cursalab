e1c3cf8cebf43f677762ce7849e02f3d0e6acbdf
Jean Carlo <jeanca2198@gmail.com>
2022-08-30 02:56:15 -0500

    API /actividad_tema_revisado

--- routes/app/topics.php
+++ routes/app/topics.php
@@ -9,4 +9,8 @@ Route::controller(RestTopicController::class)->group(function() {
     Route::get('/topics/update-plays/{topic}', 'updateTopicPlays');
     Route::get('/topics/update-resets-count/{topic}', 'updateActivity');
 
+
+    Route::get('/actividad_tema_revisado/{topic}', 'reviewTopic');
+
+
 });
