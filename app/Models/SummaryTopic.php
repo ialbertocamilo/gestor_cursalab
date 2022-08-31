cdc52fa3ca19ed04aa8c37bc24385fc85d6dfd53
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2022-08-31 13:43:59 -0500

    fix cron

--- app/Models/SummaryTopic.php
+++ app/Models/SummaryTopic.php
@@ -24,6 +24,11 @@ class SummaryTopic extends Summary
         'last_time_evaluated_at',
     ];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function topic()
     {
         return $this->belongsTo(Topic::class);
