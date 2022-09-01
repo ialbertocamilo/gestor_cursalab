29af4039d6b8bd85e9bd12e87b58fc80983b7fe9
Jean Carlo <jeanca2198@gmail.com>
2022-09-01 12:48:55 -0500

    Fix API : /evaluar_preguntas

--- app/Models/SummaryCourse.php
+++ app/Models/SummaryCourse.php
@@ -10,7 +10,9 @@ class SummaryCourse extends Summary
     protected $table = 'summary_courses';
 
     protected $fillable = [
-        'last_time_evaluated_at', 'user_id', 'course_id', 'status_id', 'assigned', 'attempts', 'views', 'advanced_percentage', 'grade_average', 'passed', 'taken', 'reviewed', 'failed', 'completed', 'restarts', 'restarter_id', 'certification_issued_at',
+        'last_time_evaluated_at', 'user_id', 'course_id', 'status_id', 'assigned', 'attempts',
+        'views', 'advanced_percentage', 'grade_average', 'passed', 'taken', 'reviewed', 'failed',
+        'completed', 'restarts', 'restarter_id', 'certification_issued_at',
     ];
 
     public function course()
@@ -189,6 +191,12 @@ class SummaryCourse extends Summary
 
         // Porcentaje avance por curso
         $assigned = count($active_topics);
+        info($course->name);
+        info("PASSED :: ". $passed);
+        info("TAKEN :: ". $taken);
+        info("REVISADOS :: ". $reviewed);
+        info("ASSIGNED ". $assigned);
+        info("q_completed ". $q_completed);
         $advanced_percentage = ($assigned > 0) ? (($q_completed / $assigned) * 100) : 0;
         $advanced_percentage = ($advanced_percentage > 100) ? 100 : $advanced_percentage; // Maximo porcentaje = 100
 
