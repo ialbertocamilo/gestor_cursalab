ed8133b54c6675b266b69e46e82b9dad74cc0a89
Elvis Fernandez <elvis@cursalab.io>
2022-09-02 17:26:39 -0500

    Header is now centered in workspaces list

--- resources/views/workspaces/list.blade.php
+++ resources/views/workspaces/list.blade.php
@@ -5,7 +5,11 @@
 @endsection
 
 @section('content')
-    @include('layouts.user-header')
+    <div class="row bg-white justify-content-center">
+        <div class="col-10">
+            @include('layouts.user-header')
+        </div>
+    </div>
     <v-app>
         <workspaces-list-layout></workspaces-list-layout>
     </v-app>
