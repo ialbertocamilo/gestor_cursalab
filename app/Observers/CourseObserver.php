79ed88c4297d386066f8bc08ad028dc9d8c0bd70
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2022-09-02 09:21:48 -0500

    test

--- app/Observers/CourseObserver.php
+++ app/Observers/CourseObserver.php
@@ -9,7 +9,7 @@ class CourseObserver
     /**
      * Handle the Course "created" event.
      *
-     * @param  \App\Models\Course  $course
+     * @param  \App\Models\Course $course
      * @return void
      */
     public function created(Course $course)
@@ -20,18 +20,23 @@ class CourseObserver
     /**
      * Handle the Course "updated" event.
      *
-     * @param  \App\Models\Course  $course
+     * @param  \App\Models\Course $course
      * @return void
      */
     public function updated(Course $course)
     {
-        //
+        if ( $course->isDirty('active') ) {
+
+            // Ocultar si ya no le pertenece
+            SummaryCourse::where('course_id', $course_id)->update(['hidden' => !$course->active]);
+
+        }
     }
 
     /**
      * Handle the Course "deleted" event.
      *
-     * @param  \App\Models\Course  $course
+     * @param  \App\Models\Course $course
      * @return void
      */
     public function deleted(Course $course)
@@ -42,7 +47,7 @@ class CourseObserver
     /**
      * Handle the Course "restored" event.
      *
-     * @param  \App\Models\Course  $course
+     * @param  \App\Models\Course $course
      * @return void
      */
     public function restored(Course $course)
@@ -53,7 +58,7 @@ class CourseObserver
     /**
      * Handle the Course "force deleted" event.
      *
-     * @param  \App\Models\Course  $course
+     * @param  \App\Models\Course $course
      * @return void
      */
     public function forceDeleted(Course $course)
