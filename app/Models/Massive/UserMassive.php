cc0432a4a4f1bca404d1fc86b1c66315c13a32d5
Aldo Lopez <aldo@lamediadl.com>
2022-09-13 11:51:13 -0500

    colocar Debug en UsuerMassive

--- app/Models/Massive/UserMassive.php
+++ app/Models/Massive/UserMassive.php
@@ -131,6 +131,7 @@ class UserMassive implements ToCollection
             }
         }
         $user['criterion_list_final'] = $user['criterion_list'];
+        dd($user);
         return compact('has_error','user','errors_index');
     }
     private function process_header($headers,$criteria){
