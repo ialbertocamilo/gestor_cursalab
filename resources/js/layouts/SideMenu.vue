4f1dca6951be370c3c2f45974853297c57c65348
Jean Carlo <jeanca2198@gmail.com>
2022-09-30 18:52:45 -0500

    Hide NotasPorCurso Report

--- resources/js/layouts/SideMenu.vue
+++ resources/js/layouts/SideMenu.vue
@@ -390,7 +390,7 @@ export default {
                         permission: "reportes",
                         role: [
                             "super-user",
-                            "admin-TEMPORAL_INACTIVO",
+                            "admin",
                             "trainer-TEMPORAL_INACTIVO",
                             "reports-TEMPORAL_INACTIVO"
                         ]
