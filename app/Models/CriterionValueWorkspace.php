f3f175bfe6e3ae41c99860cf9a11e33b7b6db234
Juan Cabanillas <juan@cursalab.io>
2022-10-07 10:05:12 -0500

    Aulas Virtuales: segmentacion 1

---
+++ app/Models/CriterionValueWorkspace.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CriterionValueWorkspace extends Model
+{
+    use HasFactory;
+    protected $table = 'criterion_value_workspace';
+
+/*    public function criterion() {
+        return $this->hasMany();
+    }
+*/
+}
