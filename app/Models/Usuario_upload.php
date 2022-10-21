e1d862b9b4ff71cb97d14b2230ec583c12188782
Juan Cabanillas <juan@cursalab.io>
2022-10-21 10:40:42 -0500

    fillable Usuario_upload

--- app/Models/Usuario_upload.php
+++ app/Models/Usuario_upload.php
@@ -7,6 +7,8 @@ class Usuario_upload extends BaseModel
 {
     protected $table = 'usuario_uploads';
 
+    protected $fillable = ['usuario_id', 'subworkspace_id', 'file', 'link', 'descripcion'];
+
     /* AUDIT TAGS */
     public function generateTags(): array
     {
