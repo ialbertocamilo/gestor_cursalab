63a3a4a1f42e2501143038c9ed4cf21609f9ac06
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2022-10-25 11:51:06 -0500

    fix uc fape login

--- app/Http/Controllers/ApiRest/AuthController.php
+++ app/Http/Controllers/ApiRest/AuthController.php
@@ -64,8 +64,13 @@ class AuthController extends Controller
         // Stop login to users from specific workspaces 
         $this->checkForMaintenanceModeSubworkspace($user->subworkspace_id);
 
-        if ($user->subworkspace_id == 29 AND $user->external_id)
-            return $this->error("Usuario inactivo temporalmente. Migración en progreso.", http_code: 401);
+        if ($user->subworkspace_id == 29 AND $user->external_id) {
+            
+            // return $this->error("Usuario inactivo temporalmente. Migración en progreso.", http_code: 401);
+            return $this->error(
+                    config('errors.maintenance_subworkspace_message'), 503
+                );
+        }
 
         if (!$user->active)
             return $this->error("Usuario inactivo.", http_code: 401);
