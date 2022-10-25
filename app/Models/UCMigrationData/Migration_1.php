ae76f35df27ed0346530427a89a9bbc173c9a457
Jean Carlo <jeanca2198@gmail.com>
2022-10-25 18:40:17 -0500

    Migration_1::fixSubworkpsaceIdUsers();

--- app/Models/UCMigrationData/Migration_1.php
+++ app/Models/UCMigrationData/Migration_1.php
@@ -1139,10 +1139,14 @@ class Migration_1 extends Model
 
                     info("El usuario {$user->id} tiene el criterion_value {$wrong_sub_workspace_value->id} y se le va a cambiar por {$correct_sub_workspace_value}");
 
-                    DB::table('criterion_value_user')
+                    $row = DB::table('criterion_value_user')
                         ->where('user_id', $user->id)
-                        ->where('criterion_value_id', $wrong_sub_workspace_value)
-                        ->update(['criterion_value_id' => $correct_sub_workspace_value]);
+                        ->where('criterion_value_id', $wrong_sub_workspace_value->id)
+                        ->first();
+
+//                        ->update(['criterion_value_id' => $correct_sub_workspace_value]);
+
+                    info($row);
                 }
 
             });
