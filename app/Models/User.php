dedb2b764b7ac03e1d7600b6b91b371cb6eeff61
Aldo Lopez <aldo@lamediadl.com>
2022-10-26 14:17:09 -0500

    Fix merge main-testing

--- app/Models/User.php
+++ app/Models/User.php
@@ -696,6 +696,7 @@ class User extends Authenticatable implements Identifiable, Recordable, HasMedia
                 endif;
             }
         }
+        unset($user->active_cycle);
     }
 
     public function validateUCCyclesRule(Segment $segment, $user): bool
