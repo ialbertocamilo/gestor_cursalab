62c7a7ccad769e20e9b64a7358e7f763f93f0482
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2022-10-28 19:09:46 -0500

    small fix

--- resources/views/meetings/list.blade.php
+++ resources/views/meetings/list.blade.php
@@ -4,7 +4,7 @@
     @include('layouts.user-header')
     <v-app>
         {{-- <meetings-layout/> --}}
-        <meetings-layout :usuario_id="{{ Auth::user()->id }}" :workspace_id="{{ get_current_workspace()->id  }}" :superuser="{{ Auth::user()->isAn('super-user') ? true : false  }}"/>
+        <meetings-layout :usuario_id="{{ Auth::user()->id }}" :workspace_id="{{ get_current_workspace()->id  }}" {{ Auth::user()->isAn('super-user') ? 'superuser' : ''  }}/>
         {{-- <aulas-virtuales-layout :usuario_id="{{ Auth::user()->id }}" :workspace_id="1"/> --}}
     </v-app>
 @endsection
