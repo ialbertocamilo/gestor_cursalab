6c571b2ca646e5fe620d4343736b93f489f366ee
Jean Carlo <jeanca2198@gmail.com>
2022-11-16 17:53:13 -0500

    Fix ranking

--- routes/app/ranking.php
+++ routes/app/ranking.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\ApiRest\RestRankController;
 
 Route::controller(RestRankController::class)->group(function() {
 
-    Route::get('/', 'ranking_v2');
+    Route::get('/', 'ranking');
+
+    Route::get('/v2', 'ranking_v2');
 
     Route::get('/criterion-code/{type}', 'rankingByCriterionCode');
 
