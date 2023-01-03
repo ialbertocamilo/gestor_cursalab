4b68234a2b4ca0eb8443ba9b01c32cb6306a29d1
Aldo Lopez <aldo@lamediadl.com>
2023-01-02 20:17:13 -0500

    FIX - considerer status por iniciar in reinicios programados

--- app/Console/Commands/reinicios_programado.php
+++ app/Console/Commands/reinicios_programado.php
@@ -89,8 +89,15 @@ class reinicios_programado extends Command
 
         $this->findCoursesToBeReset();
         $courses = $this->coursesWorkspaces;
+        
         // Reset attempts and update reset count
-        $summary_topics_id = SummaryTopic::where('passed',0)->where('attempts','>',1)->whereHas('topic',function($q) use($courses){
+        $desaprobado = Taxonomy::getFirstData('topic', 'user-status', 'desaprobado');
+        //Some users use the quiz attempt but don't finish it, so the summary topic has the status to-start
+        $por_iniciar = Taxonomy::getFirstData('topic', 'user-status', 'por-iniciar');
+        $summary_topics_id = SummaryTopic::
+        // where('passed',0)
+        whereIn('status_id', [$desaprobado->id,$por_iniciar->id])
+        ->where('attempts','>',1)->whereHas('topic',function($q) use($courses){
             $q->whereIn('course_id',array_column($courses,'courseId'));
         })->groupBy('topic_id')->select('topic_id')->pluck('topic_id');
         $courses_id = Topic::whereIn('id',$summary_topics_id)->where('active',1)->select('course_id')->pluck('course_id');
