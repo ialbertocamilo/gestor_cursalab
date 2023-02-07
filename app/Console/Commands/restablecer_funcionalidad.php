368644784f3a4048518fac9aafeba471cb9619e5
Aldo Lopez <aldo@lamediadl.com>
2023-02-07 15:53:30 -0500

    command to get info users from json

--- app/Console/Commands/restablecer_funcionalidad.php
+++ app/Console/Commands/restablecer_funcionalidad.php
@@ -113,7 +113,7 @@ class restablecer_funcionalidad extends Command
                         'documento'=>$user->document,
                         'workspace'=>$user->subworkspace->parent->name,
                         'subworkspace'=>$user->subworkspace->name,
-                        'created_at'=>$user->created_at
+                        'created_at'=>Carbon::parse($user->created_at)->format('Y-m-d H:m:s')
                     ];
                 }
             }
