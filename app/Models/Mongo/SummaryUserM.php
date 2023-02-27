a76177cc65758d1edc1636fb76252fe87a88f053
Aldo Lopez <aldo@lamediadl.com>
2023-02-27 09:36:48 -0500

    save summary_user in mongodb

--- app/Models/Mongo/SummaryUserM.php
+++ app/Models/Mongo/SummaryUserM.php
@@ -2,15 +2,26 @@
 
 namespace App\Models\Mongo;
 
-use Carbon\Carbon;
-use App\Models\Topic;
-use Carbon\CarbonPeriod;
 use Illuminate\Support\Facades\DB;
 use Jenssegers\Mongodb\Eloquent\Model;
 
-class ViewsM extends Model
+class SummaryUserM extends Model
 {
     protected $connection = 'mongodb';
-    protected $collection = 'summary_user';
+    protected $collection = 'summary_users';
+
+    public static function store(){
+        $now_date = now()->format('Y-m-d');
+        for ($i=0; $i < 31; $i++) { 
+            info($i);
+            DB::table('summary_users')->select('id','last_time_evaluated_at', 'courses_assigned', 'user_id', 'attempts', 'score',
+            'grade_average', 'courses_completed', 'advanced_percentage')->chunkById(40000, function ($summary_users) use($now_date){
+                foreach ($summary_users as $summary) {
+                    $summary->generated_date = $now_date; 
+                    self::insert((array) $summary);
+                }
+            });
+        }
+    }
 }
 ?>
