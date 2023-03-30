661e8115cdaed103f486f77a729a0f357eff6880
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2023-03-30 13:44:25 -0500

    test rate limit 600 web

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -52,6 +52,11 @@ class RouteServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(600)->by($request->user()?->id ?: $request->ip());
         });
+
+        RateLimiter::for('web', function (Request $request) {
+            return Limit::perMinute(600)->by($request->user()?->id ?: $request->ip());
+        });
+
         // RateLimiter::for('integrations', function (Request $request) {
         //     return Limit::perMinute(300)->by($request->user()?->id ?: $request->ip());
         // });
