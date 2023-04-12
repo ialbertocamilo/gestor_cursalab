08882a177163f1ee87c53aaeff53fed960bf7c21
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2023-04-12 11:08:58 -0500

    small fixes

--- resources/js/components/globals/DefaultAlertDialog.vue
+++ resources/js/components/globals/DefaultAlertDialog.vue
@@ -43,7 +43,7 @@ export default {
         },
         cancelLabel: {
             type: String,
-            default: 'Retroceder'
+            default: 'Cancelar'
         },
         confirmLabel: {
             type: String,
