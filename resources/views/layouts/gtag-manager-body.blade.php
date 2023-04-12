988369fd84167b43c280a818280128924d9cfb19
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2023-04-12 18:29:29 -0500

    gtag manager test

--- resources/views/layouts/gtag-manager-body.blade.php
+++ resources/views/layouts/gtag-manager-body.blade.php
@@ -5,25 +5,3 @@
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M7JT8H2"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-
-<script>
-
-var host = window.location.hostname;
-
-if( host == "gestiona.inretail.cursalab.io" )
-{
-	@if( auth()->check() )
-
-		console.log('authenticated datalayer')
-		
-		window.dataLayer = window.dataLayer || [];
-
-		window.dataLayer.push({
-		   'userId' : "{{ auth()->user()->id }}",
-		   'event': 'authenticated',
-		})
-
-	@endif
-}
-
-</script>
