f7c0c885b9661e3ff1093cd734f7b843f4da40a3
Aldo Lopez <aldo@lamediadl.com>
2023-06-06 12:49:13 -0500

    sending emails of apis information

--- resources/views/emails/plantilla_email.blade.php
+++ resources/views/emails/plantilla_email.blade.php
@@ -43,7 +43,7 @@
                                                 style="color: white;text-decoration: none;">
                                                 <img 
                                                     {{-- src="https://cursalab.io/wp-content/uploads/2022/11/logo-web-light-1536x506.png" --}}
-                                                    src="/img/logo_cursalab_v2_black.png"
+                                                    src="{{ url('/img/logo_cursalab_v2_black.png') }}"
                                                     alt="Cursalab" width="150"
                                                     style="max-width: 150;height:auto;border:none;">
                                             </a>
