ff889b796356963579ca07116e6fceebc968487c
juan <juan@cursalab.io>
2023-07-05 10:04:55 -0500

    juan: messages login values

--- app/Rules/CustomContextSpecificWords.php
+++ app/Rules/CustomContextSpecificWords.php
@@ -7,10 +7,13 @@ use LangleyFoxall\LaravelNISTPasswordRules\Rules\ContextSpecificWords;
 class CustomContextSpecificWords extends ContextSpecificWords
 {
     protected $identifier;
+    protected $value;
 
     public function __construct($value, $identifier)
     {
         parent::__construct($value);
+
+        $this->value = $value;
         $this->identifier = $identifier;
     }
 
@@ -18,13 +21,13 @@ class CustomContextSpecificWords extends ContextSpecificWords
     {
         switch ($this->identifier) {
             case 'document':
-                return "No puedes incluir tu Documento de Identidad en la contraseña. Usa otros términos que te sean fácil de recordar.";
+                return "No puedes incluir tu Documento de Identidad ".'"'.$this->value.'"'." en la contraseña.\nUsa otros términos que te sean fácil de recordar.";
             case 'name':
-                return "No puedes incluir tu nombre en la contraseña. Usa otros términos que te sean fáciles de recordar.";
+                return "No puedes incluir tu nombre ".'"'.$this->value.'"'." en la contraseña.\nUsa otros términos que te sean fáciles de recordar.";
             case 'lastname':
-                return "No puedes incluir tu nombre ni apellido en la contraseña. Usa otros términos que te sean fácil de recordar.";
+                return "No puedes incluir tu nombre ni apellido ".'"'.$this->value.'"'." en la contraseña.\nUsa otros términos que te sean fácil de recordar.";
             case 'surname':
-                return "No puedes incluir tu nombre ni apellido en la contraseña. Usa otros términos que te sean fácil de recordar.";
+                return "No puedes incluir tu nombre ni apellido ".'"'.$this->value.'"'." en la contraseña.\nUsa otros términos que te sean fácil de recordar.";
             default:
                 return parent::message();
         }
