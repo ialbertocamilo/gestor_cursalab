6575a49c06496f3217cbed82a326b1ca911ce746
Crusbel <crusbeljl@gmail.com>
2023-07-05 17:40:45 -0500

    Crusbel: Ajustes Beneficios

--- resources/js/layouts/SideMenu.vue
+++ resources/js/layouts/SideMenu.vue
@@ -640,11 +640,16 @@ export default {
                 }
             });
             new_grupos.forEach(sec => {
-                vue.functionality.forEach(f => {
-                    if(sec.functionality.includes(f)) {
-                        new_grupos_sections.push(sec)
-                    }
-                });
+                if (!sec.hasOwnProperty('functionality')) {
+                    new_grupos_sections.push(sec)
+                }
+                else {
+                    vue.functionality.forEach(f => {
+                        if (sec.functionality.includes(f)) {
+                            new_grupos_sections.push(sec)
+                        }
+                    });
+                }
             });
             // console.log(new_grupos);
             return new_grupos_sections;
