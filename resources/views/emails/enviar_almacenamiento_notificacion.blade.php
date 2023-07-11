db22c80b838c2af837effd6c55205eabfff7016d
juan <juan@cursalab.io>
2023-07-11 13:05:45 -0500

    juan:test markup email

--- resources/views/emails/enviar_almacenamiento_notificacion.blade.php
+++ resources/views/emails/enviar_almacenamiento_notificacion.blade.php
@@ -2,25 +2,29 @@
 @section('body')
     <tr style="background-color:#F9FAFB; text-align:start; margin-top:10px;">
         <td style="font-family: 'Poppins', sans-serif; ">
-            <h3 style="color:#5458EA; font-size: 1.5rem;">
-                Solicitud de aumento de capacidades.
-            </h3>
-            <p style="font-weight: bold;">Administrador: {{ $data['user_admin'] }}</p>
-            <p>Correo: {{ $data['user_admin_email'] }} </p>
-            <p>Workspace: {{ $data['workspace_name'] }}</p>
-
-            <div style="margin-bottom: 2rem; margin-top: 2rem;">
-                <p style="margin-bottom: 1rem;"> 
-                    <img src="{{ url('/img/storage/cloud-outline.png') }}" alt="storage"> Aumentar almacenamiento: 
-                    <b>{{ $data['storage'] }} Gb.</b> 
+            <div style="margin: 0 1rem;">
+                <h3 style="color:#5458ea;font-size:1.4rem;margin-top: 0.5rem;">
+                    Solicitud de aumento de capacidades.
+                </h3>
+                <p style="font-weight:bold;margin: 0.3rem 0;">Administrador: {{ $data['user_admin'] }}</p>
+                <p style="margin: 0.3rem 0;">
+                    Correo: {{ $data['user_admin_email'] }}</a> 
                 </p>
-                <p>
-                    <img src="{{ url('/img/storage/users-outline.png') }}" alt="users"> Aumentar usuarios: 
-                    <b>{{ $data['users'] }} usuarios.</b> 
+                <p style="margin: 0.3rem 0;">
+                    Workspace: {{ $data['workspace_name'] }}
                 </p>
+                <div style="margin-bottom: 1rem;margin-top: 1rem;">
+                    <p style="margin: 0.3rem 0;display: flex;align-items: center;"> 
+                        <img src="{{ url('/img/storage/cloud-outline.png') }}" style="margin-right: 0.3rem;"> Aumentar almacenamiento: 
+                        <b>8 Gb.</b> 
+                    </p>
+                    <p style="margin: 0.3rem 0;display: flex;">
+                        <img src="{{ url('/img/storage/users-outline.png') }}" style="margin-right: .3rem;"> Aumentar usuarios: 
+                        <b>{{ $data['users'] }} usuarios.</b> 
+                    </p>
+                </div>
+                <p> <span style="font-weight:bold">Descripción: </span> {{ $data['description'] }} </p>
             </div>
-
-            <p> <span style="font-weight:bold;">Descripción:</span> {{ $data['description'] }} </p>
         </td>
     </tr>
 @endsection
