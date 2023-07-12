5e294daa559a6f680a59fcf90d3f91c95c4617c8
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2023-07-12 13:09:24 -0500

    beta and coming-soon tags

--- resources/js/components/globals/DefaultModalSection.vue
+++ resources/js/components/globals/DefaultModalSection.vue
@@ -2,6 +2,9 @@
     <v-card flat tile elevation="0" style="border-radius: 4px !important;">
         <v-card-title class="default-section-title d-flex align-items-center align-center" >
             <p class="default-section-title-p" v-text="title"/>
+            <div v-if="comingSoon" class="coming-soon">
+                <small>*Próximamente</small>
+            </div>
             <DefaultInfoTooltip
                 v-if="tooltip"
                 class="ml-1"
@@ -22,7 +25,8 @@ export default {
             type: String,
             default: 'Título de la sección'
         },
-        tooltip: String
+        tooltip: String,
+        comingSoon: false,
     }
 }
 </script>
