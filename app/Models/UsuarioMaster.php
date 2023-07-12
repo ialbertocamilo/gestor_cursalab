917b2f681d7f4ea50bc46479c9d772055c12af3f
AdrianEsqueiros <u201718169@upc.edu.pe>
2023-07-12 18:25:15 -0500

    adrian: crud usuarios en master

--- app/Models/UsuarioMaster.php
+++ app/Models/UsuarioMaster.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsuarioMaster extends Model
 {
+    protected $connection = 'mysql_master';
     protected $table = 'master_usuarios';
     public $timestamps = false;
     protected $fillable = [
     	'dni','email','username','customer_id','created_at', 'updated_at', 'delete_at'
     ];
 
-    protected $connection = 'mysql_master';
 
 }
