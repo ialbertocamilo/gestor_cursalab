bf5f8a77e95521db5e2e8fac9d92730e53f0869a
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2023-08-17 12:02:56 -0500

    hide report reconocimiento and delete workspace button

--- resources/js/layouts/Workspaces/WorkspacesList.vue
+++ resources/js/layouts/Workspaces/WorkspacesList.vue
@@ -235,8 +235,9 @@
                                             'delete',
                                             `Eliminar workspace - ${workspace.name}`
                                         )"
-                                        v-show="!view && workspace.is_super_user"
+                                        v-show="false"
                                     >
+                                        <!-- v-show="!view && workspace.is_super_user" -->
                                         <span class="v-badge">
                                             <v-icon class="icon" color="primary">mdi-delete</v-icon>
                                             <br> <span class="table-default-icon-title" v-text="'Eliminar'"/>
