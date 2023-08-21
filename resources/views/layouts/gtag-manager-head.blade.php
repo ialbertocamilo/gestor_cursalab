074cbcb92036a6b6ae6a5dc635ec781bb2258796
Aldo Lopez <aldo@lamediadl.com>
2023-08-21 07:30:38 -0500

    new command to restart attemps in summary_topics

--- resources/views/layouts/gtag-manager-head.blade.php
+++ resources/views/layouts/gtag-manager-head.blade.php
@@ -1,4 +1,4 @@
-{{-- <script>
+<script>
 
 var host = window.location.hostname;
 
@@ -25,4 +25,4 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-M7JT8H2');</script>
 <!-- End Google Tag Manager -->
- --}}
+
