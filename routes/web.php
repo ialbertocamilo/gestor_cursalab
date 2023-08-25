40e19e414f92b9e16a254a60e0570c012c8e51fc
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2023-08-25 17:46:15 -0500

    small fixes positions text

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CursosController;
 use App\Http\Controllers\GestorController;
 use App\Http\Controllers\GeneralController;
+use App\Http\Controllers\DiplomaController;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\WorkspaceController;
