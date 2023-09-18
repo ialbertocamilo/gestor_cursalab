4725e096452c3acd608e93147f312e200ec09f41
Elvis Fernandez <vfernandez.backoffice@gmail.com>
2023-09-18 17:04:51 -0500

    Elvis: Link for app has been in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -83,7 +83,27 @@
 
 
             <br>
-            <a href="{{ route('password.update') }}" target="_self" rel="noopener noreferrer" style="color:#0000D2" class="pt-4">Olvidé mi contraseña</a>
+            <a href="{{ route('password.update') }}" target="_self" rel="noopener noreferrer" style="color:#5f59eb;" class="pt-4">
+                <b>Olvidé mi contraseña</b>
+            </a>
+
+            <p class="pt-4">
+                <svg role="presentation"
+                     viewBox="0 0 24 24"
+                     style="height: 1.5rem; width: 1.5rem; margin-right: 0.5rem">
+                    <title>account-arrow-right</title>
+                    <path d="M18 16H14V18H18V20L21 17L18 14V16M11 4C8.8 4 7 5.8 7 8S8.8 12 11 12 15 10.2 15 8 13.2 4 11 4M11 14C6.6 14 3 15.8 3 18V20H12.5C12.2 19.2 12 18.4 12 17.5C12 16.3 12.3 15.2 12.9 14.1C12.3 14.1 11.7 14 11 14" style="fill: #5f59eb;"></path>
+                </svg>
+
+                ¿Necesitas entrar como colaborador?
+                <a href="{{ config('app.web_url') }}"
+                   style="color: #5f59eb; text-decoration: underline">
+                    <b>
+                        haz click aquí
+                    </b>
+                </a>
+            </p>
+
           </div>
         </form>
       </div>
