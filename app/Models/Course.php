04415cdaa1eba471457479a9df811c9e258a4c4e
Aldo Lopez <aldo@lamediadl.com>
2023-09-18 12:31:24 -0500

    aldo: set code status project in api to get courses and topics

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -808,6 +808,7 @@ class Course extends BaseModel
                 if($project){
                     $status_project = $status_projects->where('project_id',$project->id)->where('user_id',$user->id)->first();
                     $project->status = $status_project?->status?->name ?? 'Pendiente';
+                    $project->code = $status_project?->status?->code ?? 'pending';
                     $project->available = $course_status['available'];
                     unset($project->course_id);
                 }
