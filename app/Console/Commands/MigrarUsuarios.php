8029e208a7b0be130fcce2a78d828d6c6e46388f
AdrianEsqueiros <u201718169@upc.edu.pe>
2023-10-19 17:04:54 -0500

    fix migrarUsuarios.php

--- app/Console/Commands/MigrarUsuarios.php
+++ app/Console/Commands/MigrarUsuarios.php
@@ -37,38 +37,43 @@ class MigrarUsuarios extends Command
         $destino = DB::connection('mysql_master');
 
         // Obtener los usuarios de la base de datos de origen
-        // $usuarios = $origen->select('SELECT document,email,username FROM users where subworkspace_id > 33 and subworkspace_id not in (66, 88, 115, 166, 167, 168) and type_id = 4554;');
-        $usuarios = $origen->select('SELECT document,email,username FROM users where subworkspace_id > 33 and subworkspace_id not in (66, 88, 115, 166, 167, 168) and type_id = 4554 	and document not in ("12345678");');
-        // Migrar cada usuario a la base de datos de destino
+        $usuarios = $origen->select("SELECT document, email, username FROM users WHERE subworkspace_id IS NOT NULL AND type_id IN (select id from taxonomies t where t.group='user' and t.type='type' and t.code='client');");
+
         foreach ($usuarios as $usuario) {
             // Verificar si el usuario ya existe en la base de datos de destino
             $existingUser = $destino
                 ->table('master_usuarios')
-                ->where('dni', $usuario->document)
-                // ->orWhere('email', $usuario->email)
+                ->where(function ($query) use ($usuario) {
+                    $query->where('dni', $usuario->document)
+                        ->orWhere('email', $usuario->email);
+                })
                 ->first();
-            info('Usuario existente: ' . $usuario->document . $usuario->email);
-            if (!$existingUser) {
-                if ($usuario->email == '') {
-                    $destino->table('master_usuarios')->insert([
+
+            if ($existingUser) {
+                // Si el usuario ya existe, salta al siguiente usuario
+                info('Usuario ya existe: ' . $usuario->document . $usuario->email??'');
+                continue;
+            }
+
+            if ($usuario->email == '') {
+                $destino->table('master_usuarios')->insert([
                     'dni' => $usuario->document,
                     'username' => $usuario->username,
-                    'email' => 'sin_correo'.$correo_nulo++,
+                    // 'email' => 'sin_correo' . $correo_nulo++,
                     'customer_id' => ENV('CUSTOMER_ID'),
                 ]);
                 info('Usuario migrado: ' . $usuario->document);
-                } else {
-
-                    $destino->table('master_usuarios')->insert([
-                        'dni' => $usuario->document,
-                        'username' => $usuario->username,
-                        'email' => $usuario->email,
-                        'customer_id' => ENV('CUSTOMER_ID'),
-                    ]);
-                    info('Usuario migrado: ' . $usuario->document);
-                }
+            } else {
+                $destino->table('master_usuarios')->insert([
+                    'dni' => $usuario->document,
+                    'username' => $usuario->username,
+                    'email' => $usuario->email,
+                    'customer_id' => ENV('CUSTOMER_ID'),
+                ]);
+                info('Usuario migrado: ' . $usuario->document);
             }
         }
+
         $this->info('Usuarios migrados con éxito.');
         return Command::SUCCESS;
     }
