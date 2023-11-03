b926a75cc57e915413a3ec7d960f4c7707c67232
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2023-11-03 18:40:29 -0500

    rcf: improve mediaprogress command - temp timestamps false

--- app/Models/SummaryTopic.php
+++ app/Models/SummaryTopic.php
@@ -22,6 +22,8 @@ class SummaryTopic extends Summary
         'answers' => 'array',
     ];
 
+    // public $timestamps = false;
+
     public $defaultRelationships = [
         'topic_id' => 'topic',
         'user_id' => 'user'
