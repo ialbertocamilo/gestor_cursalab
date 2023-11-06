0c5a95e03871ce0a5264b398451be8c007dbe45e
Aldo Lopez <aldo@lamediadl.com>
2023-11-06 18:22:13 -0500

    aldo: command to create menu and submenu

--- app/Console/Commands/SetMenu.php
+++ app/Console/Commands/SetMenu.php
@@ -6,6 +6,7 @@ use App\Models\Role;
 use App\Models\Ability;
 use App\Models\Taxonomy;
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
 
 class SetMenu extends Command
 {
@@ -14,20 +15,32 @@ class SetMenu extends Command
      *
      * @var string
      */
-    protected $signature = 'set:menu';
+    // protected $signature = 'set:menu';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    // protected $description = 'create menu or submenu';
 
     /**
      * Execute the console command.
      *
      * @return int
      */
+    protected function configure()
+    {
+        $this->setName('set:menu')
+            ->setDescription('create menu or submenu')
+            ->addOption('name_menu', null, InputOption::VALUE_OPTIONAL, 'name taxonomy menu')
+            ->addOption('icon_menu', null, null, 'icon taxonomy menu')
+            ->addOption('name_submenu', null, InputOption::VALUE_OPTIONAL, 'name taxonomy submenu')
+            ->addOption('code_submenu', null, InputOption::VALUE_OPTIONAL, 'code taxonomy submenu')
+            ->addOption('icon_submenu', null, InputOption::VALUE_OPTIONAL, 'icon taxonomy submenu')
+            ->addOption('path', null, InputOption::VALUE_OPTIONAL, 'path submenu')
+            ->addOption('subpaths', null, InputOption::VALUE_OPTIONAL, 'subpaths submenu');
+    }
     public function handle()
     {
         $this->setMenu();
@@ -35,23 +48,45 @@ class SetMenu extends Command
 
     private function setMenu(){
         $roles_to_sync = [];
+        $parameters_required = [
+            'name_menu',
+            'name_submenu',
+            'code_submenu',
+            'icon_submenu',
+            'path',
+            'subpaths'
+        ];
+        $data['name_menu'] = $this->input->getOption('name_menu');
+        $data['icon_menu'] = $this->input->getOption('icon_menu');
+        $data['name_submenu'] = $this->input->getOption('name_submenu');
+        $data['code_submenu'] = $this->input->getOption('code_submenu');
+        $data['icon_submenu'] = $this->input->getOption('icon_submenu');
+        $data['path'] = $this->input->getOption('path');
+        $data['subpaths'] = explode(',',$this->input->getOption('subpaths'));
+        foreach ($parameters_required as $parameter) {
+            if(!isset($data[$parameter])){
+                $this->info('Es necesario el parámetro '.$parameter);
+                $this->info("php artisan set:menu --name_menu='USUARIO' --name_submenu='Invitados' --code_submenu='guests' --icon_submenu='fa fa-users' --path='invitados' --subpaths='invitados'");
+                return;
+            }
+        }
         $menus = [
             [
                 'group'=>'gestor',
                 'type' => 'menu',
                 'position'=>1,
-                'name' => "RESUMEN",
-                'icon' => "fas fa-dice-d6",
+                'name' =>  $data['name_menu'],
+                'icon' => $data['icon_menu'],
                 'children' => [
                     [
                         'group'=>'gestor',
                         'type' => 'submenu',
-                        'code' => 'projects',
-                        'name' => "Tareas",
-                        'icon' => "fa fa-home",
+                        'code' => $data['code_submenu'],
+                        'name' => $data['name_submenu'],
+                        'icon' =>  $data['icon_submenu'],
                         'extra_attributes'=>[
-                            'path'=> "/projects",
-                            'subpaths' => ["projects"],
+                            'path'=> "/".$data['path'],
+                            'subpaths' => $data['subpaths'],
                         ],
                         'abilities' =>[
                             [
@@ -80,6 +115,9 @@ class SetMenu extends Command
                     'show_upgrade'=> $menu['show_upgrade'] ?? false,
                 ];
                 $new_menu->save();
+                $this->info('Se creó el menú '.$menu['name']);
+            }else{
+                $this->info('El menú ya estaba creado '.$menu['name']);
             }
             
             foreach ($menu['children'] as $index_submenu => $children) {
@@ -95,6 +133,9 @@ class SetMenu extends Command
                     $submenu->icon = $children['icon'];
                     $submenu->extra_attributes = $children['extra_attributes'];
                     $submenu->save();
+                    $this->info('Se creó el submenú '.$children['name']);
+                }else{
+                    $this->info('El submenú ya estaba creando '.$children['name']);
                 }
                 $abilities_id = [];
                 foreach ($children['abilities'] as $children_ability) {
@@ -108,6 +149,7 @@ class SetMenu extends Command
                     }
                     $abilities_id[] = $ability->id;
                 }
+                $this->info('Se crearon las habilidades');
                 foreach ($children['roles'] as  $role_name) {
                     if(isset($roles_to_sync[$role_name])){
                         $roles_to_sync[$role_name] = array_merge($roles_to_sync[$role_name],$abilities_id);
@@ -115,7 +157,9 @@ class SetMenu extends Command
                         $roles_to_sync[$role_name] = $abilities_id;
                     }
                 }
+                $this->info('Se asigno el submenú al superusuario');
             }
+
         }
         foreach ($roles_to_sync as $role_name => $abilities_id) {
             $role = Role::where('name',$role_name)->first();
