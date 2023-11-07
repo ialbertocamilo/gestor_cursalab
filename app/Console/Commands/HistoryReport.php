96abce01263f513f489c5878b1d3d2474ae63df6
Elvis Fernandez <elvis@cursalab.io>
2023-11-07 12:56:13 -0500

    Scheduled history report is working

---
+++ app/Console/Commands/HistoryReport.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Workspace;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class HistoryReport extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reports:history';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $url = rtrim(env('REPORTS_BASE_URL'), '/') . '/exportar/users_history';
+        $this->info($url);
+
+        $workspace = Workspace::where('slug', 'super-food-holding-peru')->first();
+        $modulesIds = Workspace::query()
+            ->where('active', 1)
+            ->where('parent_id', $workspace->id)
+            ->get()
+            ->pluck('id')
+            ->toArray();
+
+        foreach ($modulesIds as $moduleId) {
+            $response = Http::acceptJson()->post($url, [
+                'workspaceId' => $workspace->id,
+                'modules' => [$moduleId]
+            ]);
+
+            $this->info($response->getStatusCode());
+        }
+
+       // dd($response);
+       // dd($response->getStatusCode());
+       // dd($response->getBody()->getContents());
+
+        return Command::SUCCESS;
+    }
+}
