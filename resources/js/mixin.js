b60b5106fda5cdd03427171a1deccd2422cff028
Aldo Lopez <aldo@lamediadl.com>
2023-11-28 15:20:57 -0500

    aldo: fix download excel errors from upload massive in excel

--- resources/js/mixin.js
+++ resources/js/mixin.js
@@ -43,7 +43,7 @@ export default {
             mixin_extensiones: extensiones,
             mixin_default_media_images: default_media_images,
             abc: [
-                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T","U", "V", "W", "X", "Y", "Z","AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AU", "AV", "AW", "AX", "AY", "AZ",
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T","U", "V", "W", "X", "Y", "Z","AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS","AT", "AU", "AV", "AW", "AX", "AY", "AZ",
             ],
         }
     },
