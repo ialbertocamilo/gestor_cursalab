c59b2a929f136c18f3b8266af15d2a8f3e92ecfc
Rodrigo CF <rodrigo.callanaupa@gmail.com>
2023-12-12 12:33:47 -0500

    rcf: upload criterion values by excel js v

--- resources/views/layouts/appback.blade.php
+++ resources/views/layouts/appback.blade.php
@@ -306,8 +306,8 @@ if (isset($fullScreen)) {
         const USER_WORKSPACE_SLUG = "{{ $workspace?->slug }}";
     </script>
     <script src="{{ asset('js/sweetalert2.js') }}"></script>
-    <script src="{{ asset('js/app.js?v=3.2390-' . date('Y-W-d-H')) }}"></script>
-    <script src="{{ asset('js/custom.js?v=3.2390-' . date('Y-W-m')) }}"></script>
+    <script src="{{ asset('js/app.js?v=3.2391-' . date('Y-W-d-H')) }}"></script>
+    <script src="{{ asset('js/custom.js?v=3.2391-' . date('Y-W-m')) }}"></script>
 
     <script>
         $(document).ready(function () {
