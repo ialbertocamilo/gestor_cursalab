18673ab6cc6bf72603c27a60f3cd4b0b1b03f0e4
Elvis Fernandez <elvis@cursalab.io>
2023-12-15 11:33:00 -0500

    finishes_at has been returned to its previous value

--- app/Http/Controllers/ApiRest/RestQuizController.php
+++ app/Http/Controllers/ApiRest/RestQuizController.php
@@ -175,8 +175,8 @@ class RestQuizController extends Controller
             'tipo_evaluacion' => $topic->evaluation_type->code ?? NULL,
             'attempt' => [
                 'started_at' => $row->current_quiz_started_at->format('Y/m/d H:i'),
-                'finishes_at' => now()->addHours(24)->format('Y/m/d H:i'),//$row->current_quiz_finishes_at->format('Y/m/d H:i'),
-                'diff_in_minutes' => now()->diffInMinutes(now()->addHours(24))//$row->current_quiz_finishes_at),
+                'finishes_at' => $row->current_quiz_finishes_at->format('Y/m/d H:i'),
+                'diff_in_minutes' => now()->diffInMinutes($row->current_quiz_finishes_at),
             ],
         ];
 
