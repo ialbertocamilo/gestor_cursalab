982767431824c5c169ec194de5c8f6acb4bb9a67
Aldo Lopez <aldo@lamediadl.com>
2024-02-07 16:56:47 -0500

    aldo: merge into main

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -654,7 +654,6 @@ class Topic extends BaseModel
                     'estado_tema_str' => $topic_status_arr[$topic_status['status']],
                     'mod_evaluaciones' => $course->getModEvaluacionesConverted($topic),
                     'tags' => $topic->tags->map( fn($t) => $t->taxonomy),
-                    'require_view_all_media' =>true,
                     'review_all_duration_media' => boolval($topic->review_all_duration_media)
                 ]);
             }
