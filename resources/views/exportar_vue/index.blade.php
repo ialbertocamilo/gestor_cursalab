1d49650f2d7f134687c4c4ba7d3fcc17f8e11647
cursalabdev <tecnologia@cursalab.io>
2024-03-26 19:21:03 -0500

    message commit

--- resources/views/exportar_vue/index.blade.php
+++ resources/views/exportar_vue/index.blade.php
@@ -3,14 +3,15 @@
 @section('content')
 @include('layouts.user-header')
 <section class="section-list">
-  <header class="page-header">
+  <!--
+    <header class="page-header">
     <div class="container-fluid">
       <div class="d-flex flex-row align-items-center justify-content-between">
         <h5 class="no-margin-bottom">Reportes</h5>
       </div>
     </div>
   </header>
-
+  -->
   <div class="mt-3">
     <reportes-layout></reportes-layout>
   </div>
