d5912869820b1e4067762da919201d3a3838ef1f
Aldo Lopez <aldo@lamediadl.com>
2024-04-01 12:59:26 -0500

    aldo: logs in duplicate workspaces

--- app/Models/Workspace.php
+++ app/Models/Workspace.php
@@ -775,7 +775,7 @@ class Workspace extends BaseModel
         $this->load($relationships);
 
         $_crit_module = Criterion::where('code', 'module')->first();
-
+        info('duplicate polls');
         foreach ($this->polls as $_poll) {
 
             $poll = $workspace->polls()->create($_poll->toArray());
@@ -784,6 +784,7 @@ class Workspace extends BaseModel
         }
 
         $workspace->refresh();
+        info('duplicate subworkspaces');
 
         foreach ($this->subworkspaces as $_subworkspace) {
 
@@ -886,6 +887,7 @@ class Workspace extends BaseModel
             }
         }
 
+        info('duplicate criterionWorkspace');
 
         $workspace->functionalities()->sync($this->functionalities);
 
@@ -911,7 +913,7 @@ class Workspace extends BaseModel
 
         $modules_ids = $workspace->subworkspaces()->pluck('criterion_value_id')->toArray();
         $subworkspace_ids = $workspace->subworkspaces()->pluck('id')->toArray();
-
+        info('duplicate _announcements');
         foreach ($_announcements as $_announcement) {
 
             $announcement = Announcement::create($_announcement->toArray());
@@ -919,6 +921,7 @@ class Workspace extends BaseModel
             $announcement->criterionValues()->sync($modules_ids);
         }
         //Duplicate Videoteca
+        info('duplicate videotecas');
         foreach ($this->videotecas as $_videoteca) {
 
             $_videoteca_data = $_videoteca->toArray();
@@ -933,17 +936,21 @@ class Workspace extends BaseModel
             $videoteca->modules()->sync($subworkspace_ids);
             // $videoteca->tags()->sync($_videoteca->tags);
         }
+        info('duplicate duplicateChecklistFromWorkspace');
         //Duplicate FUNCTIONALITIES
         $duplicate_functionalities = json_decode($data['duplicate']);
         if(isset($duplicate_functionalities->checklist) && $duplicate_functionalities->checklist){
             Checklist::duplicateChecklistFromWorkspace($this,$workspace);
         }
+        info('duplicate duplicateBenefitsFromWorkspace');
         if(isset($duplicate_functionalities->benefits) && $duplicate_functionalities->benefits){
             Benefit::duplicateBenefitsFromWorkspace($this,$workspace);
         }
+        info('duplicate duplicateCertificatesFromWorkspace');
         if(isset($duplicate_functionalities->certificates) && $duplicate_functionalities->certificates){
             Certificate::duplicateCertificatesFromWorkspace($this,$workspace);
         }
+        info('duplicate duplicateVademecumsFromWorkspace');
         if(isset($duplicate_functionalities->protocols_and_documents) && $duplicate_functionalities->protocols_and_documents){
             $current_modules_ids = $this->subworkspaces->pluck('id');
             Vademecum::duplicateVademecumsFromWorkspace($current_modules_ids,$workspace);
